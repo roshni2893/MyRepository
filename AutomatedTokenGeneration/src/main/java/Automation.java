203388b3ab784091dae93ef5e5e7a718d5076142
Amita <Amita@192.168.2.7>
2017-09-09 16:58:39 +0530

    Step 2 requirements...

--- AutomatedTokenGeneration/src/main/java/Automation.java
+++ AutomatedTokenGeneration/src/main/java/Automation.java
@@ -30,6 +30,30 @@ public class Automation {
 		
 		
 		// Step2
+		// url : https://psd2.apiboidev.com/1/api/open-banking/v1.0/account-requests
+		// headers:
+		// Content-Type : application/json
+		//Authorization : clientCredAccessToken variable
+//		x-fapi-financial-id : 123
+//		client_id : 6443e15975554bce8099e35b88b40465
+//		client_secret : 7ca382cf4dff4d2dBCF034DBBA34A653
+//		BODY : {
+//			"Data": {
+//			"Permissions": [
+//				"ReadAccountsDetail",
+//
+//				"ReadTransactionsCredits",
+//
+//				"ReadTransactionsDetail"
+//			],
+//			"ExpirationDateTime": "2018-01-19T00:00:00.875",
+//			"TransactionFromDateTime": "2014-01-19T00:00:00.800",
+//			"TransactionToDateTime": "2017-01-19T00:00:00.345"
+//		},
+//		"Risk": {}
+//	}
+		
+		
 		
 		// Step3
 		
@@ -52,9 +76,9 @@ public class Automation {
 		System.exit(0);
 	}
 
-	static String getValueFromGetApiCall(String url, MultiValueMap<String, String> queryParams,
+	static String getValueFromGetApiCall(String url, MultiValueMap<String, String> queryParamsMap,
 			Map<String, String> queryHeaders, String requiredValue) throws IOException {
-		UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(url).queryParams(queryParams);
+		UriComponentsBuilder builder = UriComponentsBuilder.fromUriString(url).queryParams(queryParamsMap);
 		UriComponents finalUri = builder.build();
 		RestTemplate rt = new RestTemplate();
 		HttpHeaders headers = new HttpHeaders();
