6242b045d2e1b7fcfbec36251efc051f4f0a9114
Amita <Amita@192.168.2.7>
2017-09-09 18:42:57 +0530

    step3 req

--- AutomatedTokenGeneration/src/main/java/Step3.java
+++ AutomatedTokenGeneration/src/main/java/Step3.java
@@ -3,6 +3,15 @@ public class Step3 {
 
 	public static String step3(String accounRequestId) {
 		return null;
+		
+		
+		// https://psd2.apiboidev.com/openbanking-nOAuth/oauth/authorize?
+//		grant_type=authorization_code&
+//				client_id=6443e15975554bce8099e35b88b40465&scope=accounts&
+//				brandId=ROI&
+//				AccountRequestId=da36080f-7377-410f-97f2-f05ca03f179a&
+//				redirect_uri=https://www.getpostman.com/oauth2/callback&
+//				response_type=code
 	}
 
 }
